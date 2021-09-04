470ab83e28d137e13a957cc0f3ec1e5a3c38464d
YogeshLather <yogeshlather25111996@gmail.com>
2021-09-04 20:14:09 +0530

    Adding initial project setup

---
+++ myProjects/src/test/java/com/example/myProjects/SchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myProjects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
